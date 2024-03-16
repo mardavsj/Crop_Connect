dca07f553f8be727f9bed9aed8d5393871293568
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2024-03-16 07:15:58 +0530

    Pricing Plan

--- fontend/src/components/Herosection.tsx
+++ fontend/src/components/Herosection.tsx
@@ -7,14 +7,14 @@ const Herosection = () => {
       <MaskContainer
         revealText={
           <p className="max-w-4xl mx-auto text-white text-center  text-4xl font-bold">
-         With Tech in Hand,Every Crop has a Plan.
-         Sowing Tech For Happy Harvest's :Crop-Connect
+         With Tech in Hand Every Crop has a Plan.
+         Sowing Tech For Happy Harvest s :Crop-Connect
           </p>
         }
         className="h-[40rem] border rounded-md"
       >
-        With Tech in   <span className="text-primary-hackathon-color-500">Hand,Every Crop</span> Chas a Plan,
-        Sowing Tech For Happy Harvest's<span className="text-primary-hackathon-color-500 hover:text-5xl">:Crop-Connect</span>.
+        With Tech in   <span className="text-primary-hackathon-color-500">Hand Every Crop</span> Chas a Plan
+        Sowing Tech For Happy Harvest s<span className="text-primary-hackathon-color-500 hover:text-5xl">:Crop-Connect</span>.
       </MaskContainer>
     </div>
     </>
